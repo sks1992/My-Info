f92a6d7066147263f6861ad6af853a439d370c3b
sandeep <sksandeep9215@gmail.com>
2021-05-29 06:45:13 +0530

    simple intro and hobby app

---
+++ app/src/main/java/sandeep/kumar/aboutme/MainActivity.kt
@@ -0,0 +1,61 @@
+package sandeep.kumar.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import sandeep.kumar.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Binding object for MainActivity.
+    // Name of the object is derived from the name of the activity or fragment.
+    private lateinit var binding: ActivityMainBinding
+
+    // Instance of MyName data class.
+    private val myName: MyName = MyName("Aleks Haecky")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Setting the content view using DataBindingUtil creates an instance of
+        // ActivityMainBinding from the supplied activity and the supplied layout. This object
+        // contains mappings between the activity and layout,
+        // and functionality to interact with them.
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // Set the value of the myName variable that is declared and used in the layout file.
+        binding.myName = myName
+
+        // Click listener for the Done button.
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    /**
+     * Click handler for the Done button.
+     * Demonstrates how data binding can be used to make code much more readable
+     * by eliminating calls to findViewById and changing data in the binding object.
+     */
+    private fun addNickname(view: View) {
+        binding.apply {
+            // Set the text for nicknameText to the value in nicknameEdit.
+            myName?.nickname = nicknameEdit.text.toString()
+            // Invalidate all binding expressions and request a new rebind to refresh UI
+            invalidateAll()
+            // Change which views are visible.
+            // Remove the EditText and the Button.
+            // With GONE they are invisible and do not occupy space.
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+
+            // Make the TexView with the nickname visible.
+            nicknameText.visibility = View.VISIBLE
+        }
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
---
+++ app/src/main/java/sandeep/kumar/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package sandeep.kumar.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
